d6fffec91be4cebdb1a33129a521affdeba9856b
Ashé <papaom@ashes-mbp.lan>
2022-06-07 16:13:32 -0400

    connected server to sql database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,6 +10,23 @@ app.use(express.json());
 
 app.use((req, res) => {
   res.status(404).end();
+});
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: '127.0.0.1',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'whole',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.listen(PORT, () => {
